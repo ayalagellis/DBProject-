43557472f8270d65a691baf68903ae758d67c0dd
ayalagellis <100130564+ayalagellis@users.noreply.github.com>
2024-05-30 00:15:25 +0300

    Add files via upload

---
+++ Shlav1/SelectAll.sql
@@ -0,0 +1,8 @@
+select * from person;
+select * from communitymember;
+select * from external_donor;
+select * from donation;
+select * from birth;
+select * from community;
+select * from Spending;
+
